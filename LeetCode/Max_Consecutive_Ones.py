32b459b5357a10268662d8077368307cf706696f
ramushetty <ummadishettyramu@gmail.com>
2022-12-29 21:08:15 +0530

    max consecutive ones

---
+++ LeetCode/Max_Consecutive_Ones.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+
+        max_count = count = 0
+
+        for i in nums:
+            if i == 1:
+                count +=1
+
+            else:
+                max_count = max(count,max_count)
+                count = 0
+        return max(count,max_count)
