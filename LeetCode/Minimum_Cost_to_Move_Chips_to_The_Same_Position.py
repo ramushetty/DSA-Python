f3d71c84f7315620ba47f030e97cc7452e71b824
ramushetty <ummadishettyramu@gmail.com>
2023-01-12 07:35:58 +0530

    minimum cost to move chips to the same postion

---
+++ LeetCode/Minimum_Cost_to_Move_Chips_to_The_Same_Position.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even_count = 0
+        odd_count = 0
+        for p in position:
+            if p % 2 == 0:
+                even_count += 1
+            else:
+                odd_count += 1
+
+        return min(even_count,odd_count)
