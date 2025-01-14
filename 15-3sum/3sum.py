3a9055ec2e0659b79baf919e02c24f1175f35e6d
Ramu Ummadishetty <41884544+ramushetty@users.noreply.github.com>
2025-01-14 18:09:05 +0530

    Time: 561 ms (66.77%) | Memory: 20.8 MB (26.84%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,40 +1,65 @@
 class Solution:
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums_len = len(nums)
-        result = []
-        if nums_len < 3:
-            return []
+    #     nums_len = len(nums)
+    #     result = []
+    #     if nums_len < 3:
+    #         return []
 
-        # n1 + n2 + n3 = 0
-        # n1 + n2 = -n3 both are same
+    #     # n1 + n2 + n3 = 0
+    #     # n1 + n2 = -n3 both are same
         
-        nums.sort() # sort the nums
+    #     nums.sort() # sort the nums
 
-        # fix one element n1
-        for i in range(0, nums_len-2):
-            # remove duplicate
-            if i !=0 and nums[i] == nums[i-1]:
+    #     # fix one element n1
+    #     for i in range(0, nums_len-2):
+    #         # remove duplicate
+    #         if i !=0 and nums[i] == nums[i-1]:
+    #             continue
+            
+    #         self.twoSum(nums, -nums[i], result, i+1)
+    #     return result
+    # def twoSum(self,nums,target,result, start_index):
+
+    #     i = start_index
+    #     j = len(nums)-1
+    #     while i < j:
+    #         elements_sum = nums[i] + nums[j]
+    #         if elements_sum > target:
+    #             j -=1
+    #         elif elements_sum < target:
+    #             i +=1
+    #         else:
+    #             result.append([-target,nums[i],nums[j]])
+    #             while i<j and nums[i] == nums[i+1]: i+=1
+    #             while i<j and nums[j] == nums[j-1]: j-=1
+    #             i+=1
+    #             j-=1
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            self.twoSum(nums, -nums[i], result, i+1)
-        return result
-    def twoSum(self,nums,target,result, start_index):
+            j = i + 1
+            k = len(nums) - 1
 
-        i = start_index
-        j = len(nums)-1
-        while i < j:
-            elements_sum = nums[i] + nums[j]
-            if elements_sum > target:
-                j -=1
-            elif elements_sum < target:
-                i +=1
-            else:
-                result.append([-target,nums[i],nums[j]])
-                while i<j and nums[i] == nums[i+1]: i+=1
-                while i<j and nums[j] == nums[j-1]: j-=1
-                i+=1
-                j-=1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
+        
+        return res
 
 
         
