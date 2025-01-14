a7a3b030fc61f29fb7d18c6adb822541e5f19740
Ramu Ummadishetty <41884544+ramushetty@users.noreply.github.com>
2025-01-14 20:55:58 +0530

    Time: 27 ms (12.29%) | Memory: 21.2 MB (5.35%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        m = len(mat)
+        n = len(mat[0])
+
+        map_diag = {}
+        for i in range(m):
+            for j in range(n):
+                # use i+j technique as it is diagonal travesal 
+                if i+j in map_diag:
+                    map_diag[i+j].append(mat[i][j])
+                else:
+                    map_diag[i+j] = [mat[i][j]]
+        result = []
+        print(map_diag)
+        for key in map_diag.items():
+            if key[0] % 2 == 0:
+                for e in key[1][::-1]:
+                    result.append(e)
+            else:
+                for e in key[1]:
+                    result.append(e)
+        return result
+
+
+            
