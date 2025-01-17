58ef3d5ccb393ad39c82b64ab7f977dc2dd241b0
Ramu Ummadishetty <41884544+ramushetty@users.noreply.github.com>
2025-01-17 21:08:44 +0530

    Time: 19 ms (42.90%) | Memory: 19.1 MB (30.39%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,17 +1,14 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        mp = {}
-        for element in nums:
-            mp[element] = mp.get(element,0) + 1
-        duplicate = 0
-        for key,value in mp.items():
-            if value == 2:
-                duplicate = key
-                break
-        missing  = 0
-        for index in range(1,len(nums)+1):
-            if index not in nums:
-                missing  = index
-                break
+        duplicate , missing = -1, -1
+
+        for index in range(len(nums)):
+            if nums[abs(nums[index])-1] < 0:
+                duplicate = abs(nums[index])
+            else:
+                nums[abs(nums[index])-1]  *=-1
+        for index in range(len(nums)):
+            if nums[index] > 0:
+                missing = index+1
         return [duplicate,missing]
         
