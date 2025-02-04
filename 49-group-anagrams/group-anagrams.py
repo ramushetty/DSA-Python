fb6860431307003d3c365231d49bb1818dd84db8
Ramu Ummadishetty <41884544+ramushetty@users.noreply.github.com>
2025-02-04 16:04:14 +0530

    Time: 35 ms (9.22%) | Memory: 20.9 MB (54.54%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         h_map = {}
-        print(strs)
         for word in strs:
-            temp = ''.join(sorted(word))
-            # print(temp)
-
-            if temp in h_map:
-                h_map[temp].append(word)
+            char_array = [0]*26
+            for c in word:
+                char_array[ord(c)-ord('a')] +=1
+            new_word = ''
+            for i in range(26):
+                if  char_array[i] > 0:
+                    temp = chr(ord('a')+i)
+                    for c in range(char_array[i]):
+                        new_word+=temp
+            if new_word in h_map:
+                h_map[new_word].append(word)
             else:
-                h_map[temp] = [word]
+                h_map[new_word] = [word]
+
         result = []
         for item in h_map.items():
             result.append(item[1])
-        return result
-        
+        return result 
